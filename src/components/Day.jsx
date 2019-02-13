f70d0938bacb56234cf769229c881d78067b41a7
Stuart Gill <stuart.a.gill@gmail.com>
2019-02-13 15:58:29 -0800

    inline styling for day component

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -4,12 +4,17 @@ import PropTypes from "prop-types";
 function Day(props) {
   return (
     <div>
-      <h3>
-        {props.name}
-        {props.location}
-        {props.hours}
-        {props.booth}
-      </h3>
+      <style jsx>
+        {`
+          p {
+            margin-left: 30px;
+          }
+        `}
+      </style>
+      <h2>{props.name}</h2>
+      <p>{props.location}</p>
+      <p>{props.hours}</p>
+      <p>{props.booth}</p>
     </div>
   );
 }
